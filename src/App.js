06b2f316d182b270bd85464afae000b9717dff5d
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-09 15:07:53 -0300

    loading components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Form from './components/Form';
 import Navbar from './components/Navbar';
 import VideoBg from './components/VideoBg';
-
+import Registered from './components/Registered';
 
 function App() {
+
+
   return (
     <div className="App">
       <Navbar />      
       <VideoBg />
       <Form />
+      <Registered />     
     </div>
   );
 }
